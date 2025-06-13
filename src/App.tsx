f7779db9b76376216279e55f6ee06098d624d39b
Malik Ibo <malik97un@gmail.com>
2025-06-13 23:46:32 +0200

    Home page et Index.html

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router"
 function App() {
   
   return ( 
-    <main>
+    <main className="bg-gray-200 min-h-screen">
       <Outlet />
     </main>
   
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router";
+
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <section className="container mx-auto flex flex-col items-center justify-center min-h-screen">
+      <article className="bg-white text-center p-10 rounded-md shadow-md">
+        <h1 className="text-3xl mb-6">GESTION DES NAISSANCES</h1>
+        <Link
+          to={"/private/declaration"}
+          className="border-4 rounded-md border-blue-700 text-blue-700 px-46 py-4 hover:bg-blue-600 hover:text-white transition-colors duration-300"
+        >
+          Déclaration
+        </Link>
+      </article>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
