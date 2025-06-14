19b78571ec8d7f1bb1d47b3910b2a5de1d50ee76
Malik Ibo <malik97un@gmail.com>
2025-06-14 20:25:14 +0200

    Sav. pour teste un button

--- src/components/declarations/DeclarationsItems.tsx
+++ src/components/declarations/DeclarationsItems.tsx
@@ -1,9 +1,10 @@
 import type { Declaration } from "@/types/Declaration";
 import DeclarationArticle from "./DeclarationArticle";
+import { FaSort } from "react-icons/fa";
 
 type Props = {
   declaration: Declaration[];
-}
+};
 
 function DeclarationsItems(props: Props) {
   const { declaration } = props;
@@ -17,7 +18,12 @@ function DeclarationsItems(props: Props) {
         <span className={`p-2 `}>Hopital</span>
         <span className={`p-2  col-span-2 flex flex-col`}>Parent 1</span>
         <span className={`p-2  col-span-2 flex flex-col`}>Parent 2</span>
-        <span className={`p-2 text-center`}>Statut</span>
+        <button
+          type="button"
+          className={`p-2 text-center flex justify-between items-center`}
+        >
+          Statut <FaSort />
+        </button>
         <span className={`p-2 text-center col-span-2`}>ACTIONS</span>
       </article>
       {declaration.map((item: Declaration, index: number) => (
