54dcdd52f326d0481ffe9f337054e78272cd4827
Malik Ibo <malik97un@gmail.com>
2025-06-14 12:14:29 +0200

    Amélioration de la date

--- src/pages/Déclaration.tsx
+++ src/pages/Déclaration.tsx
@@ -4,12 +4,12 @@ function Déclaration() {
   return (
     <section className="border-2 bg-white shadow-md rounded-lg p-4">
       <article className="grid grid-cols-12 text-gray-700 font-bold col-span-2 items-center">
-        <span className={`p-2`}>Date</span>
-        <span className={`p-2 col-span-2 flex flex-col`}>Prenom Enfant</span>
-        <span className={`p-2`}>Date de Naissance</span>
-        <span className={`p-2`}>Hopital</span>
-        <span className={`p-2 col-span-2 flex flex-col`}>Parent 1</span>
-        <span className={`p-2 col-span-2 flex flex-col`}>Parent 2</span>
+        <span className={`p-2 col-span-2 flex flex-col`}>Date</span>
+        <span className={`p-2 col-span-1.5 flex flex-col`}>Prenom Enfant</span>
+        <span className={`p-2 col-span-2 flex flex-col`}>Date de Naissance</span>
+        <span className={`p-2 col-span-1.5 flex flex-col`}>Hopital</span>
+        <span className={`p-2 col-span-1.5 flex flex-col`}>Parent 1</span>
+        <span className={`p-2 col-span-1.5 flex flex-col`}>Parent 2</span>
         <span className={`p-2 text-center`}>Statut</span>
         <span>ACTIONS</span>
       </article>
@@ -20,25 +20,25 @@ function Déclaration() {
             index % 2 === 0 ? "bg-gray-100" : "bg-white"
           }`}
         >
-          <span className={`p-2`}>{FORMAT_DATE(item.registered)}</span>
-          <span className={`p-2 col-span-2 flex flex-col`}>
+          <span className={`p-2 col-span-2 flex flex-col`}>{FORMAT_DATE(item.registered)}</span>
+          <span className={`p-2 col-span-1.5 flex flex-col`}>
             <span>{item.child.firstName}</span>
             <span className="uppercase">{item.child.lastName}</span>
           </span>
-          <span className={`p-2`}>{FORMAT_DATE(item.child.brithDate)}</span>
+          <span className={`p-2 col-span-2 flex flex-col`}>{FORMAT_DATE(item.child.brithDate)}</span>
           <span className={`p-2`}>
-            <span className={`p-2`}>{item.company.name}</span>
+            <span className={`p-2 col-span-1.5 flex flex-col`}>{item.company.name}</span>
           </span>
-          <span className={`p-2 col-span-2 flex flex-col`}>
+          <span className={`p-2 col-span-1.5 flex flex-col`}>
             <span>{item.firstParent.firstName}</span>
             <span>{item.firstParent.lastName}</span>
           </span>
-          <span className={`p-2 col-span-2 flex flex-col`}>
+          <span className={`p-2 col-span-1.5 flex flex-col`}>
             <span>{item.seconParent.firstName}</span>
             <span>{item.seconParent.lastName}</span>
           </span>
           <span className={`p-3 text-center ${getStatusColor(item.status)}`}>{getStatusLabel(item.status)}</span>
-          <span className={`p-2 col-span-2 flex flex-col`}>ACTIONS</span>
+          <span className={`p-2`}>ACTIONS</span>
         </article>
       ))}
     </section>
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,6 +1,11 @@
+const dayOfWeek = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+const monthOfYear = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+const formatDay = (day: number) => {
+  return `0${day}`.slice(-2);
+}
 const FORMAT_DATE = (value: string) => {
   const date = new Date(value.split(" ")[0]);
-  return `${date.getDay()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+  return `${formatDay(date.getDay())} ${monthOfYear[date.getMonth() + 1]} ${date.getFullYear()}`;
 }
 
 export {FORMAT_DATE}
